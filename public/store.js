5d1969940f4e643f27565b08a7d386b8476f9d8d
haesooKr <gotn676@gmail.com>
2020-03-01 22:14:13 -0500

    Fetching token and itemId

--- public/store.js
+++ public/store.js
@@ -2,7 +2,26 @@ const stripeHandler = StripeCheckout.configure({
   key: stripePublicKey,
   locale: 'auto',
   token: function(token) {
-    console.log(token);
+    let selected = document.querySelector('.selected');
+    let itemId = selected.parentElement.dataset.itemId;
+
+    fetch('/purchase', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        stripeTokenId: token.id,
+        item: itemId
+      })
+    }).then(function(res){
+      return res.json()
+    }).then(function(data){
+      alert(data.message)
+    }).catch(function(error){
+      console.error(error);
+    })
   }
 })
 
@@ -11,6 +30,9 @@ const purchases = document.querySelectorAll('.shop-item button');
 purchases.forEach(item => item.addEventListener('click', purchaseClicked))
 
 function purchaseClicked(){
+  item = this.dataset.itemId;
+  this.classList.add('selected');
+
   let price = parseFloat(this.value) * 100;
   stripeHandler.open({
     amount: price
