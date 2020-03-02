b2cf85e9a99eb4653962453c420438490d72667b
haesooKr <gotn676@gmail.com>
2020-03-01 21:00:35 -0500

    Add json data control

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+console.log(stripeSecretKey, stripePublicKey);
+
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+app.get('/', function(req, res){
+  fs.readFile('items.json', function(error, data){
+    if(error){
+      res.status(500).end()
+    } else {
+      res.render('store.ejs', {
+        items: JSON.parse(data)
+      })
+    }
+  })
+})
 
 
 
