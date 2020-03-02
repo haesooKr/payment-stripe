cb81ec1f8619befe55303a1ca64f43f807739da1
haesooKr <gotn676@gmail.com>
2020-03-01 22:14:31 -0500

    Post /purchase

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ if (process.env.NODE_ENV !== 'production'){
 
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
-console.log(stripeSecretKey, stripePublicKey);
 
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const stripe = require('stripe')(stripeSecretKey);
 
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
@@ -21,6 +22,29 @@ app.get('/', function(req, res){
       res.render('store.ejs', {
         stripePublicKey,
         items: JSON.parse(data)
+      })
+    }
+  })
+})
+
+app.post('/purchase', function(req, res){
+  fs.readFile('items.json', function(error, data){
+    if(error){
+      res.status(500).end()
+    } else {
+      let dataJson = JSON.parse(data);
+      let price = dataJson.item.filter(i => i.id === Number(req.body.item));
+
+      stripe.charges.create({
+        amount: price[0].price,
+        source: req.body.stripeTokenId,
+        currency: 'usd'
+      }).then(function() {
+        console.log('Charge Successful');
+        res.json({ message: "Successfully purchased items" })
+      }).catch(function() {
+        console.log('Charge Fail');
+        res.status(500).end()
       })
     }
   })
